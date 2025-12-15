f64364dca92284ab66506828b2d05bb8c8c83620
advaitgarv <137263987+advaitgarv@users.noreply.github.com>
2025-12-15 16:58:26 +0530

    Fix leap year calculator logic and formatting

--- Javascript/Leap year calculator.js
+++ Javascript/Leap year calculator.js
@@ -1,8 +1,9 @@
 function isLeapYear(year) {
   if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
     return year + " is a leap year.";
-  } else {
-    return year + " is not a leap year.";
+  } else 
+  {
+  return year + " is not a leap year.";
   }
 }
 let year = 2024;
