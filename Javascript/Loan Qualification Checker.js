1778736b729481dd2ad966434bb5dc12dc68ff46
advaitgarv <137263987+advaitgarv@users.noreply.github.com>
2025-12-15 17:00:26 +0530

    Corrected loan qualification checker code

--- Javascript/Loan Qualification Checker.js
+++ Javascript/Loan Qualification Checker.js
@@ -1,12 +1,9 @@
 const minIncomeForDuplex = 60000;
 const minCreditScoreForDuplex = 700;
-
 const minIncomeForCondo = 45000;
 const minCreditScoreForCondo = 680;
-
 const minIncomeForCar = 30000;
 const minCreditScoreForCar = 650;
-
 function getLoanMessage(annualIncome, creditScore) {
   if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
     return "You qualify for a duplex, condo, and car loan."
@@ -18,12 +15,10 @@ function getLoanMessage(annualIncome, creditScore) {
     return "You don't qualify for any loans."
   }
 }
-
 let duplexLoanMsg = getLoanMessage(85000, 850);
 let condoLoanMsg = getLoanMessage(65000, 690);
 let carLoanMsg = getLoanMessage(45000, 660);
 let noLoanMsg = getLoanMessage(25000, 550);
-
 console.log(duplexLoanMsg);
 console.log(condoLoanMsg);
 console.log(carLoanMsg);
