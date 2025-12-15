5fe005bef4f31c4c22c19bc3d67a38fa006d7bc4
advaitgarv <137263987+advaitgarv@users.noreply.github.com>
2025-12-15 16:44:13 +0530

    Remove duplicate console.log for 30 degrees

--- Javascript/Celsius to fahrenheit converter.js
+++ Javascript/Celsius to fahrenheit converter.js
@@ -1,4 +1,3 @@
-
 function convertCtoF(celsius) {
   return (celsius * 9/5) + 32;
 }
@@ -7,5 +6,3 @@ console.log(convertCtoF(-30));
 console.log(convertCtoF(-10));
 console.log(convertCtoF(20));
 console.log(convertCtoF(30));
-
-
