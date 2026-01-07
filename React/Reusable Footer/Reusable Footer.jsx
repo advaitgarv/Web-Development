77c296be273f1f2f02ed58a75c05a69f1b06c0e9
advaitgarv <137263987+advaitgarv@users.noreply.github.com>
2026-01-07 17:35:41 +0530

    React projects

---
+++ React/Reusable Footer/Reusable Footer.jsx
@@ -0,0 +1,34 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>© 2026 My Company. All rights reserved.</p>
+
+      <ul>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Careers</a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#">Terms of Service</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
