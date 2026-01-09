b9180195aebda9e8a2f52cc85880c407a88e014e
advaitgarv <137263987+advaitgarv@users.noreply.github.com>
2026-01-09 21:36:30 +0530

    Accordian react project

---
+++ React/Accordian/src/components/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: '1',
+    question: "What are accordion components?",
+    answer:
+      "Accordion components are user interface elements used to organize and display content in expandable and collapsible sections."
+  },
+  {
+    id: '2',
+    question: "What are they used for?",
+    answer:
+      "They are commonly employed in various contexts, including FAQs, to organize large amounts of information."
+  },
+  {
+    id: '3',
+    question: "Accordion as a musical instrument",
+    answer:
+      "The accordion is a musical instrument with a keyboard and bellows that produce sound when expanded and compressed."
+  },
+  {
+    id: '4',
+    question: "Can I create an accordion component with a different framework?",
+    answer:
+      "Yes, it is very possible to create an accordion component using different frameworks or even with plain JavaScript."
+  }
+];
+export default data;
